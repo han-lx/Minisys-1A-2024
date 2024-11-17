79c63b2400398cac2506e0f10983fa4cece59512
han_lx <3119604572@qq.com>
2024-11-17 21:25:03 +0800

    完成键盘检测

--- bios.asm
+++ bios.asm
@@ -1,49 +1,54 @@
 .data 0x0000
 .text 0x0000
     start:
-        addi  $sp,$zero,0x5499   # 鍫嗘爤鎸囬拡鍒濆鍖�
-        addi  $s0,$zero,0x5000   # 瀹氫箟妫€娴嬬┖闂撮鍦板潃
-        addi  $s1,$zero,0xefff   # 妫€娴嬬┖闂寸殑缁撴潫浣嶇疆
-        addi  $s2,$zero,0xffff   # 瀹氫箟涓€涓啓鍏ュ瘎瀛樺櫒鐨勬暟鎹劧鍚庤鍑�
-        addi  $s3,$zero,0x0001   # 瀵勫瓨鍣╯3鍌ㄥ瓨閿欒鐮侊紝棰勫厛瀛樺叆ram閿欒鐮�
-        addi  $t2,$zero,0x0000   # 娴嬭瘯鏁扮爜绠�
-        addi  $t4,$zero,0x0000   # 鏁扮爜绠℃樉绀烘儏鍐�
+        addi  $sp,$zero,0x5499   # 堆栈指针初始化
+        addi  $s0,$zero,0x0500   # 确定ram检测基地址
+        addi  $s1,$zero,0xefff   # 确定ram检测结束地址
+        addi  $s2,$zero,0xffff   # 定义寄存器写入值
+        addi  $s3,$zero,0x0001   # 预先存入ram错误码
+        addi  $t2,$zero,0x0000   # 设定数位管检测初始数值
+        addi  $t4,$zero,0x0000   # 预先设定数位管检测期间不显示
         addi  $t3,$zero,0xffff
-    ram锛� # 妫€娴媌ios鍖哄煙锛屼腑鏂尯鍩熶箣澶栫殑鍖哄煙
-        lw    $t0,0($s0)         # 淇濆瓨mem棣栧湴鍧€
-        sw    $s2,0($s0)         # 灏唖2鍐欏叆鍦板潃涓簊0鐨勫瘎瀛樺櫒涓�
-        lw    $t1,0($s0)         # 璇诲彇褰撳墠鍦板潃s0涓暟鍊�
-        bne   $t1,$s2,error      # 鑻ヤ笉鐩哥瓑锛宮em瀛樺偍鍑虹幇閿欒
-        sw    $t0,0($s0)         # 鎭㈠s0棣栧湴鍧€
-        beq   $s0,$s1,ram_done       # 鑻0=s1锛屾娴嬪畬姣�
+    ram:  # 检测bios后，中断前寄存器
+        lw    $t0,0($s0)         # 预存当前检测地址
+        sw    $s2,0($s0)         # 将s2内容写入当前检测地址
+        lw    $t1,0($s0)         # 将s2内容读出到t1
+        bne   $t1,$s2,error      # 如果t1与s2不相等，跳转到error
+        sw    $t0,0($s0)         # 将t0内容写回当前检测地址
+        beq   $s0,$s1,ram_done       # 如果s0与s1相等，检测结束，跳转到ram_done
         addi  $s0,$s0,0x0004     # s0+4
-        j     ram                # 璺宠浆鍒皉am
-    ram_done:
-        addi  $v0,$zero,1   # ram妫€娴嬪畬姣�
+        j     ram                
+    ram_done: # ram检测结束
+        addi  $v0,$zero,1   # v0为状态寄存器，1为ram检测成功
         jal   done 
     digitalTube:
-        sw    $t2,0xfc00($zero)   # 鏁扮爜绠″叆鍙ｅ熀鍦板潃
-        sw    $t4,0xfc04($zero)   # 鏁扮爜绠″皬鏁扮偣
-        addiu   $t2,$t2,0x1111    # 鏁扮爜绠℃暟鍊煎悇浣�+1
-        bne   $t2,$t3,digitalTube  # 鑻ユ暟鐮佺鍊间笉绛変簬0xffff锛岀户缁娴�
-        addi  $v0,$zero,2   # 鏁扮爜绠℃娴嬪畬姣�
+        sw    $t2,0xfc00($zero)   # 数位管检测是不显示
+        sw    $t4,0xfc04($zero)   # 数位管小数点不显示
+        addiu   $t2,$t2,0x1111    # 数位管各位+1
+        bne   $t2,$t3,digitalTube  # 如果t2与t3不相等，继续循环
+        addi  $v0,$zero,2   # v0为状态寄存器，2为数位管检测成功
         jal   done
     keyboard:
-        addi  $s3,$zero,0x0002   # 閿洏閿欒鐮�
-    timer:
-    pwm:
-    watchdog:
-    led:
-    switch2:
-    buzzer:
-    error: # 楂樹綅鏄剧ず閿欒鐮�
-        addi   $t9,$zero锛�0xff00     
-        sw     $t9,0xfc04($zero)     # 璁剧疆8涓暟鐮佺鍧囨樉绀烘暟鎹�
-        sw     $s3,0xfc02($zero)     # 鏄剧ず閿欒鐮�
-    done: # 浣庝綅鏄剧ず妫€娴嬪畬姣�
-        addi   $s0,$zero锛�0xff00
-        sw     $s0,0xfc04($zero)     # 璁剧疆8涓暟鐮佺鍧囨樉绀烘暟鎹�
-        sw     $v0,0xfc00($zero)     # 鏄剧ず妫€娴嬪畬姣�
+        addi  $s3,$zero,0x0002   # 预存键盘错误码
+        addi  $s0,$zero,0        # a0储存键盘状态寄存器的值
+        addi  $s1,$zero,0        # a1储存键盘值
+        loop_start:
+            lw    $s0,0xfc12($zero)  # 将键盘状态寄存器的值存入a0，有键按下为1，无则为0
+            bne   $s0,$zero,key_presseed  # 如果状态寄存器的值不为0，跳转到key_presseed
+            j     loop_start
+        key_presseed:
+            lw    $s1,0xfc10($zero)  # 将键盘值存入a1
+            slti  $t2,$t1,0x0010     # 判断键盘值是否在正常范围内
+            addi  $v0,$zero,3   # v0为状态寄存器，3为键盘检测成功
+            bne   $t2,$zero,done      # 如果键盘值在正常范围内，跳转到done
+    error: # 检测失败，高位显示错误码
+        addi   $t9,$zero,0xff00     
+        sw     $t9,0xfc04($zero)     # 小数点不显示
+        sw     $s3,0xfc02($zero)     # 显示错误码
+    done: # 检测成功，低位显示结果
+        addi   $s0,$zero,0xff00
+        sw     $s0,0xfc04($zero)     # 小数点不显示
+        sw     $v0,0xfc00($zero)     # 显示成功码
         jr     $ra
 
 
