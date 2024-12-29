1ead4d0f12c3a8e6b2f97c14af61b49a6bbeb7fc
OverSky <seuoversky@gmail.com>
2024-12-29 17:10:08 +0800

    README

---
+++ test.c
@@ -0,0 +1,21 @@
+void delay(void) {
+  int c;
+  c = 30000;
+  while (c > 0) {
+    c = c - 1;
+  }
+  return;
+}
+void main(void) {
+  int led;
+  led = 0;
+  while (1) {
+    led = led + 1;
+    $0xfffffc60 = led;
+    $0xfffffc62 = led >> 16;
+    if (led > 10) led = 0;
+    delay();
+  }
+  return;
+}
+
